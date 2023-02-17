d73dfffa370520c70007c87781c692125af563da
Matheus <matheusroca11@gmail.com>
2023-02-16 22:28:52 -0300

    Cart styling

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,13 +1,32 @@
-import { Image, StyleSheet, Dimensions, Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 import topo from "../../assets/topo.png";
+import logo from "../../assets/logo.png"
 
 const width = Dimensions.get('screen').width;
+const height = Dimensions.get('screen').height;
 
 const Cart = () => {
     return (
         <>
             <Image source={topo} style={styles.topo} />
             <Text style={styles.titulo}>Detalhe da cesta</Text>
+
+            <View style={styles.cesta}>
+                <Text style={styles.nome}>Cesta de Verduras</Text>
+
+                <View style={styles.fazenda}>
+                    <Image source={logo} style={styles.imagemFazenda}/>
+                    <Text style={styles.nomeFazenda}>Jenny jack Farm</Text>
+                </View>
+
+                <Text style={styles.descricao}>
+                    Uma cesta com produtos selecionados 
+                    cuidadozamente da fazenda direto para 
+                    sua cozinha.
+                </Text>
+
+                <Text style={styles.preco}>R$ 40,00</Text>
+            </View>
         </>
 
     )
@@ -16,7 +35,7 @@ const Cart = () => {
 const styles = StyleSheet.create({
     topo: {
         width: "100%",
-        heigth: (578 / 1080 * width),
+        height: 578 / height * width,
     },
     titulo: {
         width: "100%",
@@ -27,7 +46,44 @@ const styles = StyleSheet.create({
         color: "white", 
         fontWeight: "bold",
         padding: 16
-    }
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    nome: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: '#464646',
+        fontWeight: "bold"
+    },
+    fazenda: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingVertical: 12,
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        fontHeight: 26,
+        marginLeft: 12
+    },
+    descricao: {
+        color: "#a3a3a3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        fontHeight: 42,
+        marginTop: 8,
+    },
+
 });
 
 export default Cart;
