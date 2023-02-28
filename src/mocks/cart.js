0febb459d3a404103999dd2ddf4dd60391736d5d
Matheus <matheusroca11@gmail.com>
2023-02-28 18:17:21 -0300

    typo correction

--- src/mocks/cart.js
+++ src/mocks/cart.js
@@ -15,7 +15,7 @@ const cart = {
         nome: "Cesta de Verduras",
         logoFazenda: logo,
         nomeFazenda: "Jenny jack Farm",
-        descricao: "Uma cesta com produtos selecionados cuidadozamente da fazenda direto para sua cozinha.",
+        descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha.",
         preco: "40,00",
         button: "Comprar"
     },
